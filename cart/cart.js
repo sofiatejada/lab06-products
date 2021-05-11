6ed322c5bc52e932574ebea4b7b63a6909564874
Sofia Tejada <sofianais@icloud.com>
2021-05-11 14:07:11 -0700

    write an array containing a objects from the products page

---
+++ cart/cart.js
@@ -0,0 +1,20 @@
+const cart = [
+    {
+        id: 'gastly',
+        quantity: 2
+    },
+    {
+        id: 'dratini',
+        quantity: 1
+    },
+    {
+        id: 'togepi',
+        quantity: 1
+    },
+    {
+        id: 'ralts',
+        quantity: 2
+    }
+];
+
+export default cart;
