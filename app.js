409bf1bd7fe1692f811652465bfb86d13d6120c0
Sofia Tejada <sofianais@icloud.com>
2021-05-14 15:28:44 -0700

    add nav button

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-// import functions and grab DOM elements
 
-// initialize state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const nav = document.querySelector('button');
+const list = document.getElementById('hide');
+
+
+console.log(nav);
+
+
+nav.addEventListener('click', () => {
+
+    list.classList.toggle('hidden');
+
+
+});
